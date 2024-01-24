a3553eae85b4edb8051dfbd06d6d744a8c086769
LorenzoEmanuele00 <lorenzoema2800@gmail.com>
2024-01-24 15:08:13 +0100

    add controll to se if the table is empty, for displaing notification

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -6,32 +6,35 @@
         <div class="text-end my-3">
             <a class="btn btn-success" href="{{route('admin.projects.create')}}">New Project</a>
         </div>
-
-        <table class="table table-striped border border-2">
-            <thead>
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Title</th>
-                    <th scope="col">Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($projects as $project)
+        @if (count($projects) > 0)
+            <table class="table table-striped border border-2">
+                <thead>
                     <tr>
-                        <th scope="row">{{$project->id}}</th>
-                        <td>{{$project->title}}</td>
-                        <td>
-                            <a class="btn btn-success" href="{{ route('admin.projects.show', ['project' => $project->slug]) }}"><i class="fa-solid fa-info"></i></a>
-                            {{-- <a class="btn btn-warning" href="{{ route('projects.edit', ['project' => $project->id]) }}"><i class="fa-solid fa-pencil"></i></a>
-                            <form action="{{ route('projects.destroy', ['project' => $project->id]) }}"" class="d-inline-block" method="POST">
-                                @csrf
-                                @method('DELETE')
-                                <button class="btn btn-danger" type="submit"><i class="fa-regular fa-trash-can"></i></button>
-                            </form>  --}}
-                        </td>
+                        <th scope="col">#</th>
+                        <th scope="col">Title</th>
+                        <th scope="col">Actions</th>
                     </tr>
-                @endforeach
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    @foreach ($projects as $project)
+                        <tr>
+                            <th scope="row">{{$project->id}}</th>
+                            <td>{{$project->title}}</td>
+                            <td>
+                                <a class="btn btn-success" href="{{ route('admin.projects.show', ['project' => $project->slug]) }}"><i class="fa-solid fa-info"></i></a>
+                                <a class="btn btn-warning" href="{{ route('admin.projects.edit', ['project' => $project->slug]) }}"><i class="fa-solid fa-pencil"></i></a>
+                                {{-- <form action="{{ route('projects.destroy', ['project' => $project->id]) }}"" class="d-inline-block" method="POST">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button class="btn btn-danger" type="submit"><i class="fa-regular fa-trash-can"></i></button>
+                                </form>  --}}
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        @else
+            <h2> Create your own Project</h2>
+        @endif
     </div>
 @endsection
